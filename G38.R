4e72ce1ebcf119af734ddbffbdba23d9a772103c
Emily Happy <emilyhappy@Emilys-MacBook-Pro.local>
2023-11-09 15:58:12 +0000

    started backward

--- G38.R
+++ G38.R
@@ -8,27 +8,29 @@ netup <- function(d) {
     h[[l]] <- c(rep(1, d[l]))
   }
   for (l in 1:(length(d)-1)){
-    W[[l]] <- matrix(runif(d[l+1]*d[l], min=0, max=2), nrow=d[l+1], ncol=d[l])
+    W[[l]] <- matrix(runif(d[l+1]*d[l], min=0, max=.2), nrow=d[l+1], ncol=d[l])
     b[[l]] <- runif(d[l+1], min=0, max=0.2)
   }
   list(h=h, W=W, b=b)
 }
 
-forward <- function(nn, inp){
+forward <- function(nn, inp) {
   for (i in 1:length(nn$h[[1]])){
     nn$h[[1]][i] <- inp[i]
   }
   for (l in 1:(length(nn$h)-1)){
     for (j in 1:length(nn$h[[l+1]])){
-      print('in loop')
-      print(l+1)
-      print(j)
       value <- (nn$W[[l]][j] * nn$h[[l]]) + nn$b[[l]][j]
       nn$h[[l+1]][j] <- max(0, value)
     }
   }
   list(h=nn$h, W=nn$W, b=nn$b)
 }
+
+backward <- function(nn,k) {
+  i = 0
+}
+
 
 d <- c(4,8,7,3)
 nn = forward(netup(d), c(1, 2, 3, 4))
