99c9722dd25b32d810a186541379c23a15fffc44
ehappy2019 <114178855+ehappy2019@users.noreply.github.com>
2023-11-09 14:54:00 +0000

    Add files via upload

---
+++ G38.R
@@ -0,0 +1,34 @@
+
+netup <- function(d) {
+  h <- list(0)
+  W <- list(0)
+  b <- list(0)
+  
+  for (l in 1:length(d)){
+    h[[l]] <- c(rep(1, d[l]))
+  }
+  for (l in 1:(length(d)-1)){
+    W[[l]] <- matrix(runif(d[l+1]*d[l], min=0, max=2), nrow=d[l+1], ncol=d[l])
+    b[[l]] <- runif(d[l+1], min=0, max=0.2)
+  }
+  list(h=h, W=W, b=b)
+}
+
+forward <- function(nn, inp){
+  for (i in 1:length(nn$h[[1]])){
+    nn$h[[1]][i] <- inp[i]
+  }
+  for (l in 1:(length(nn$h)-1)){
+    for (j in 1:length(nn$h[[l+1]])){
+      print('in loop')
+      print(l+1)
+      print(j)
+      value <- (nn$W[[l]][j] * nn$h[[l]]) + nn$b[[l]][j]
+      nn$h[[l+1]][j] <- max(0, value)
+    }
+  }
+  list(h=nn$h, W=nn$W, b=nn$b)
+}
+
+d <- c(4,8,7,3)
+nn = forward(netup(d), c(1, 2, 3, 4))
